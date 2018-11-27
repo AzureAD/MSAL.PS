54019e684cec1fb59d91ae4240b638aa2c000c6a
Jason Thompson <jasoth@microsoft.com>
2018-11-27 15:57:17 -0500

    Update

--- tests/MSAL.PS.tests.ps1
+++ tests/MSAL.PS.tests.ps1
@@ -3,7 +3,7 @@ Remove-Module MSAL.PS -ErrorAction SilentlyContinue
 Import-Module ..\src\MSAL.PS.psd1
 
 [string] $TenantId = 'jasoth.onmicrosoft.com'
-[pscredential] $PublicClient = Get-Credential '00000000-0000-0000-0000-000000000000',(ConvertTo-SecureString 'null' -AsPlainText -Force)
+[pscredential] $PublicClient = New-Object pscredential -ArgumentList '00000000-0000-0000-0000-000000000000',(ConvertTo-SecureString 'null' -AsPlainText -Force)
 if (!$ConfidentialClient) { [pscredential] $ConfidentialClient = Get-Credential -UserName '00000000-0000-0000-0000-000000000000' }
 [string[]] $Scopes = @(
     'https://graph.microsoft.com/.default'
