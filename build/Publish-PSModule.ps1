81f3d3cd360bbd533e6ed2fc1053cabfd80a0160
Jason Thompson <jasoth@microsoft.com>
2019-07-22 12:17:48 -0400

    Update Version in Publish Script

--- build/Publish-PSModule.ps1
+++ build/Publish-PSModule.ps1
@@ -2,7 +2,7 @@ param
 (
 	#
     [parameter(Mandatory=$false)]
-    [string] $ModulePath = ".\release\MSAL.PS\2.5.0.1",
+    [string] $ModulePath = ".\release\MSAL.PS\4.1.0.1",
     #
     [parameter(Mandatory=$true)]
     [string] $NuGetApiKey
