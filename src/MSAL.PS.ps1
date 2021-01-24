3a105f10c988c93aaf3c966a696ba6cd9c5d1312
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2021-01-24 12:12:10 +0100

    Adding user choice to enable dll conflict resolution

--- src/MSAL.PS.ps1
+++ src/MSAL.PS.ps1
@@ -1,3 +1,115 @@
+#region Import Helper Functions
+function Get-Config {
+    [CmdletBinding()]
+    param ()
+
+    $config = [PSCustomObject]@{
+        Mode               = 'Strict'
+        UserChose          = $false
+        ShowedInstructions = $false
+    }
+
+    $configPath = Join-Path -Path (Split-Path -Path $profile) -ChildPath 'MSAL.PS.config'
+    if (-not (Test-Path -Path $configPath)) { return $config }
+
+    $cfgFileContent = Get-Content -Path $configPath | ConvertFrom-Json
+
+    if ($cfgFileContent.PSObject.Properties.Name -contains 'Mode') { $config.Mode = $cfgFileContent.Mode }
+    if ($cfgFileContent.PSObject.Properties.Name -contains 'UserChose') { $config.UserChose = $cfgFileContent.UserChose }
+    if ($cfgFileContent.PSObject.Properties.Name -contains 'ShowedInstructions') { $config.ShowedInstructions = $cfgFileContent.ShowedInstructions }
+
+    if ($env:msalps_Mode) {
+        $config.Mode = $env:msalps_Mode
+        $config.UserChose = $true
+    }
+    if ($env:msalps_ShowedInstructions) {
+        $config.ShowedInstructions = $true
+    }
+
+    $config
+}
+
+function Set-Config {
+    [CmdletBinding()]
+    param (
+        [ValidateSet('Strict', 'Lenient')]
+        [string]
+        $Mode,
+
+        [bool]
+        $UserChose,
+
+        [bool]
+        $ShowedInstructions
+    )
+
+    $config = Get-Config
+    $configPath = Join-Path -Path (Split-Path -Path $profile) -ChildPath 'MSAL.PS.config'
+
+    if ($PSBoundParameters.Keys -contains 'Mode') { $config.Mode = $Mode }
+    if ($PSBoundParameters.Keys -contains 'UserChose') { $config.UserChose = $UserChose }
+    if ($PSBoundParameters.Keys -contains 'ShowedInstructions') { $config.ShowedInstructions = $ShowedInstructions }
+
+    $config | ConvertTo-Json | Set-Content -Path $configPath -Force -ErrorAction Ignore
+}
+
+function Get-DllImportMode {
+    [CmdletBinding()]
+    param (
+        [string]    
+        $AssemblyPath
+    )
+
+    # Option 1: Global variable
+    if (Get-Variable msalps_DllImportMode -ErrorAction Ignore) {
+        switch ($msalps_DllImportMode) {
+            'Strict' { return 'Strict' }
+            'Lenient' { return 'Lenient' }
+            default { return 'Strict' }
+        }
+    }
+
+    # Option 2: Configuration
+    $config = Get-Config
+    if ($config.UserChose) {
+        return $config.Mode
+    }
+
+    # Option 3: Ask
+    Write-Host @"
+There is already a different version of the file 'Microsoft.Identity.Client.dll' loaded:
+$($AssemblyPath)
+This MAY break our module, but it also may work just fine.
+"@
+
+    $caption = 'Continue Module Import anyway?'
+    $choseLenient = $Host.UI.PromptForChoice($caption, $null, @('&Yes', '&No'), 1) -eq 0
+    if ($choseLenient) { $mode = 'Lenient' }
+    else { $mode = 'Strict' }
+
+    $caption2 = 'Remember Choice?'
+    $message2 = "This choice will be persisted in $(Join-Path -Path (Split-Path -Path $profile) -ChildPath 'MSAL.PS.config')"
+    $persist = $Host.UI.PromptForChoice($caption2, $message2, @('&Yes', '&No'), 1) -eq 0
+
+    if ($persist) {
+        Set-Config -Mode $mode -UserChose $true
+    }
+
+    if (-not $config.ShowedInstructions) {
+        Write-Host @'
+This choice can be suppressed by setting a global variable:
+$msalps_DllImportMode = 'Strict' # Stop module import
+$msalps_DllImportMode = 'Lenient' # Continue anyway
+
+You can also define an environment variable:
+$env:msalps_mode = 'Strict' # Stop module import
+$env:msalps_mode = 'Lenient' # Continue anyway
+'@
+        Set-Config -ShowedInstructions $true
+    }
+    $mode
+}
+#endregion Import Helper Functions
 
 ## Read Module Manifest
 $ModuleManifest = Import-PowershellDataFile (Join-Path $PSScriptRoot 'MSAL.PS.psd1')
@@ -19,7 +131,18 @@ elseif ($PSEdition -eq 'Core') {
 try {
     Add-Type -LiteralPath $RequiredAssemblies | Out-Null
 }
-catch { throw }
+catch {
+    Write-Warning "Failed to load client assembly 'Microsoft.Identity.Client': $_"
+
+    $msalLib = [System.AppDomain]::CurrentDomain.GetAssemblies().Where{ 'Microsoft.Identity.Client.dll' -eq $_.ManifestModule }
+    if (-not $msalLib) { throw }
+
+    $mode = Get-DllImportMode -AssemblyPath $msalLib.Location
+    if ($mode -eq 'Strict') {
+        throw
+    }
+    $RequiredAssemblies.Add($msalLib.Location)
+}
 
 
 ## Load TokenCacheHelper
@@ -62,5 +185,5 @@ try {
     Add-Type -LiteralPath $srcDeviceCodeHelper -ReferencedAssemblies $RequiredAssemblies -IgnoreWarnings -WarningAction SilentlyContinue
 }
 catch {
-    Write-Warning 'There was an error loading some dependencies. DeviceCode paramter will not function.'
+    Write-Warning 'There was an error loading some dependencies. DeviceCode parameter will not function.'
 }
